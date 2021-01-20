1a17b2409ed8e74d5fa280d05e8b71845d95952e
mfcgames <mfethicaliskan@gmail.com>
2021-01-20 23:50:32 +0300

    20 Ocak Ders Sonu

---
+++ CampIntro/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace CampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kurs kurs1 = new Kurs();
+
+            kurs1.KursAdi = "C#";
+            kurs1.EgitmenAdi = "MFÇ";
+            kurs1.IzlenmeOrani = 68;
+
+            Kurs kurs2 = new Kurs();
+            kurs2.KursAdi = "Python";
+            kurs2.EgitmenAdi = "Engin Demiroğ";
+            kurs2.IzlenmeOrani = 100;
+
+            Kurs kurs3 = new Kurs();
+            kurs3.KursAdi = "c++";
+            kurs3.EgitmenAdi = "Ahmet";
+            kurs3.IzlenmeOrani = 85;
+
+            Kurs[] kurslar=new Kurs[]
+            {
+                kurs1,kurs2,kurs3
+            };
+            foreach (var item in kurslar)
+            {
+                Console.WriteLine(item.KursAdi,item.EgitmenAdi,item.IzlenmeOrani);
+            }
+            
+        }
+        class Kurs
+        {
+            public int IzlenmeOrani { get; set; }
+            public string EgitmenAdi { get; set; }
+            public string KursAdi { get; set; }
+        }
+    }
+}
---
+++ Metotlar/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya Elması";
+            urun1.StokAdedi = 500;
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakır Karpuzu";
+            urun2.StokAdedi = 220;
+
+            Urun[] urunler = new Urun[] {urun1,urun2};
+
+            foreach (var urun in urunler)
+            {
+                Console.WriteLine(">ÜRÜN "+urun.Adi);
+                Console.WriteLine(">FİYAT "+urun.Fiyati);
+                Console.WriteLine(">AÇIKLAMA "+urun.Aciklama);
+                Console.WriteLine("------------********************-----------");
+            }
+
+            Console.WriteLine("---------------Metotlar--------------");
+            //instance-örnek
+            //encapsulation --KAPSÜLLEME
+
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+            sepetManager.Ekle2("elma", "yeşil elma", 12,10);
+
+        }
+    }
+}
